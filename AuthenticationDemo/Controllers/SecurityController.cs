ecbc5fc88f0c97c02a9141ee42923c82fb589bbe
MUSA BALIN <20164753049@ogr.akdeniz.edu.tr>
2021-09-17 21:47:12 +0300

    Add project files.

---
+++ AuthenticationDemo/Controllers/SecurityController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using AuthenticationDemo.Models;
+
+namespace AuthenticationDemo.Controllers
+{
+    public class SecurityController : Controller
+    {
+        // GET: Security
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            var userInDb = UserInit.Init().FirstOrDefault(x => x.Username == user.Username && x.Password == user.Password);
+            if (userInDb != null)
+            {
+                FormsAuthentication.SetAuthCookie(user.Username, false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid User. Wrong Username or Password";
+                return View();
+            }
+
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Security");
+        }
+    }
+}
---
+++ AuthenticationDemo/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthenticationDemo.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Role { get; set; }
+    }
+    public class UserInit
+    {
+        public static List<User> Init()
+        {
+
+            return new List<User> {
+            new User { ID = 1,Username="Admin",Password="123",Role=0 } ,
+            new User { ID = 2,Username="admin",Password="123",Role=1 }
+        };
+        }
+    }
+}
